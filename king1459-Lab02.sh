c88af17331042bce68b3aa3703a4c1f88b316f9f
Stephanie <king1459@stthomas.edu>
2017-09-23 01:20:02 +0000

    2nd script commit

--- king1459-Lab02.sh
+++ king1459-Lab02.sh
@@ -8,6 +8,9 @@ sudo apt-get update
 sudo apt-get install -y tree
 sudo apt-get install -y git
 
-mkdir A C D A/B A/C A/D C/E C/F D/G D/H D/I D/H/J D/H/K
-touch A/aa A/B/bb A/C/cc A/D/dd C/cc C/E/ee C/F/ff D/dd D/G/gg D/H/hh D/H/J/jj D/H/K/kk D/I/ii
+cd ~
+mkdir main
+cd main
+mkdir A A/B A/C A/D E E/F E/G H H/I H/J H/J/K H/J/L H/M
+touch A/aa A/B/bb A/C/cc A/D/dd E/ee E/F/ff E/G/gg H/hh H/I/ii H/J/jj H/J/K/kk H/J/L/ll H/M/mm
 
