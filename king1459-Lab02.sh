70405a8658db65ea29148953eaa56a5554a9ef52
Stephanie <king1459@stthomas.edu>
2017-09-23 01:05:08 +0000

    script commit

---
+++ king1459-Lab02.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Starter shell script
+# Rename as YourID-Lab02.sh
+# Put commands below
+# To run, type ./YourID-Lab02.sh (you need the "./")
+# It must have permissions starting with a "7"
+sudo apt-get update
+sudo apt-get install -y tree
+sudo apt-get install -y git
+
+mkdir A C D A/B A/C A/D C/E C/F D/G D/H D/I D/H/J D/H/K
+touch A/aa A/B/bb A/C/cc A/D/dd C/cc C/E/ee C/F/ff D/dd D/G/gg D/H/hh D/H/J/jj D/H/K/kk D/I/ii
+
